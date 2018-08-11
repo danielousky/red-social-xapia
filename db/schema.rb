cc49863fd63313e6df03b6caceb4a109edafa742
Daniel Moros <moros.daniel@gmail.com>
2018-08-11 01:05:23 -0500

    Ajustes en las publicaciones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_064718) do
+ActiveRecord::Schema.define(version: 2018_08_11_053537) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.boolean "private", default: true
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
