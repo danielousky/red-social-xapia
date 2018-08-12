d8ef7e612de1b21d20362137aa17113c39f5a70e
Daniel Moros <moros.daniel@gmail.com>
2018-08-12 11:39:01 -0500

    Correccion de bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ApplicationRecord
   has_many :comments
   accepts_nested_attributes_for :comments
 
-  scope :search, lambda{|keyworks| where('name LIKE ? OR email LiKE ?', '%keyworks%', '%keyworks%')}
+  scope :search, lambda{|keyworks| where('name LIKE ? OR email LIKE ?', "%#{keyworks}%", "%#{keyworks}%")}
+  # scope :search, -> {where('name LIKE ?', '%Dan%')}
+  # scope :search, -> {where('id != ?', 3)}
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, -> { Friendship.confirmeds }, through: :friendships, source: :user
 
 end
